61b0340bb074a385388f2779dcfbe782a662a002
Autopod User <autopod@autopod.local>
2026-05-25 14:21:31 +0000

    fix(desktop): preserve legacy memory preference kind

--- packages/desktop/Sources/AutopodClient/Types/Memory.swift
+++ packages/desktop/Sources/AutopodClient/Types/Memory.swift
@@ -51,6 +51,7 @@ public enum MemoryKind: Equatable, Sendable, Codable {
   case gotcha
   case workflow
   case dependency
+  case preference
   case reviewFeedback
   case other
   case unknown(String)
@@ -62,6 +63,7 @@ public enum MemoryKind: Equatable, Sendable, Codable {
     case "gotcha": self = .gotcha
     case "workflow": self = .workflow
     case "dependency": self = .dependency
+    case "preference": self = .preference
     case "review_feedback": self = .reviewFeedback
     case "other": self = .other
     default: self = .unknown(value)
@@ -79,6 +81,7 @@ public enum MemoryKind: Equatable, Sendable, Codable {
     case .gotcha: "gotcha"
     case .workflow: "workflow"
     case .dependency: "dependency"
+    case .preference: "preference"
     case .reviewFeedback: "review_feedback"
     case .other: "other"
     case .unknown(let value): value
